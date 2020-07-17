4c7b489f68bebcb7a0b8403cbd7cd685c505021f
CIPop <Cristian.Pop@microsoft.com>
2020-07-17 16:30:36 -0700

    Adding very simple HSM.

--- sdk/samples/iot/reference/src/paho_iot_reference_example.c
+++ sdk/samples/iot/reference/src/paho_iot_reference_example.c
@@ -31,10 +31,67 @@
 // Clients
 static az_iot_provisioning_client provisioning_client;
 
+enum
+{
+  STATE_ENTER = 0x1,
+  STATE_EXIT = 0x2,
+};
+
+struct hsm
+{
+  struct hsm * super_hsm;
+  az_result (*currentState)(struct hsm *this, int event_type, void* event_data);
+  // no queue, one event only.
+} test_hsm;
+
+static az_result initial_state(struct hsm *this, int event_type, void* event_data);
+static az_result state_a(struct hsm *this, int event_type, void* event_data);
+
+static az_result initial_state(struct hsm *this, int event_type, void* event_data)
+{
+  az_result ret = AZ_OK;
+
+  switch(event_type)
+  {
+    case STATE_ENTER:
+      LOG_SUCCESS("initial_state: event STATE_ENTER");
+
+      // Change states w/o queue:
+      this->currentState = state_a;
+      this->currentState(this, STATE_ENTER, NULL);
+      break;
+
+    default:
+      ret = this->super_hsm->currentState(this->super_hsm, event_type, event_data);
+  }
+
+  return ret;
+}
+
+static az_result state_a(struct hsm *this, int event_type, void* event_data)
+{
+  az_result ret = AZ_OK;
+
+  switch(event_type)
+  {
+    case STATE_ENTER:
+      LOG_SUCCESS("state_a: event STATE_ENTER");
+      break;
+
+    default:
+      ret = this->super_hsm->currentState(this->super_hsm, event_type, event_data);
+  }
+
+  return ret; 
+}
+
 
 int main()
 {
   LOG_SUCCESS("Started.");
+
+  test_hsm.currentState = initial_state;
+  test_hsm.currentState(&test_hsm, STATE_ENTER, NULL);
 
   return 0;
 }
